629b42e289fa8a8f7d852322de66a8acd6ae73ca
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-04-14 14:24:47 +0200

    Update leboncoin_pro.py

--- leboncoin_pro.py
+++ leboncoin_pro.py
@@ -1,5 +1,8 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright
 import re
+import random
+import time
 
 def get_leboncoin_prices(query, km=None, carburant=None, boite=None):
 
@@ -7,20 +10,34 @@ def get_leboncoin_prices(query, km=None, carburant=None, boite=None):
 
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
+
+            browser = p.chromium.launch(
+                headless=True,
+                args=["--disable-blink-features=AutomationControlled"]
+            )
+
+            context = browser.new_context(
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
+            )
+
+            page = context.new_page()
 
             url = f"https://www.leboncoin.fr/recherche?category=2&text={query}"
+
             page.goto(url, timeout=60000)
 
-            # ⏳ attendre chargement annonces
-            page.wait_for_selector("a[data-qa-id='aditem_container']", timeout=15000)
+            # ⏳ simulation humaine
+            time.sleep(random.uniform(2, 4))
 
-            annonces = page.query_selector_all("a[data-qa-id='aditem_container']")
+            # scroll pour charger annonces
+            page.mouse.wheel(0, 2000)
+            time.sleep(2)
 
-            for annonce in annonces:
+            elements = page.query_selector_all("a")
+
+            for el in elements:
                 try:
-                    text = annonce.inner_text()
+                    text = el.inner_text()
 
                     match = re.search(r"(\d{3,6})\s?€", text)
                     if match:
