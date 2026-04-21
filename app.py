0ee31e12de316dcd243585b3830e1d097f6988ef
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-04-21 13:23:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ BASE_PRICES = {
 
     "volkswagen golf 2019": 22000, "volkswagen golf 2022": 26000, "volkswagen golf 2023": 27000,
     "volkswagen tiguan 2020": 33000, "volkswagen tiguan 2022": 34700, "volkswagen tiguan 2023": 35700,
-    "volkswagen t cross 2020": 22700,
-    "volkswagen t cross": 22700,
+    "volkswagen t cross 2020": 22600,
+    "volkswagen t cross": 22600,
     
     "renault zoe": 22000,
     "renault zoe 2021": 20000,
