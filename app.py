76bbd34e109352be55349ddbf14a37780f8a2e05
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-04-24 11:14:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ BASE_PRICES = {
     "mercedes classe a": 36000,
     "mercedes classe a 2020": 31000, "mercedes classe a 2015": 22000, "mercedes classe a 2022": 33000, "mercedes classe a 2023": 34000,
     "mercedes gla": 42000,
-    "mercedes classe b": 40000,
+    "mercedes classe b": 52000,
 
     "volkswagen golf 2019": 22000, "volkswagen golf 2022": 26000, "volkswagen golf 2023": 27000,
     "volkswagen tiguan 2020": 33000, "volkswagen tiguan 2022": 34700, "volkswagen tiguan 2023": 35700,
