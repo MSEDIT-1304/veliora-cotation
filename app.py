6b9aad2fda142c9919c739fd89a6f86fadd0dc27
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-05-04 20:15:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -576,8 +576,36 @@ if st.session_state.admin_logged:
 
 if not st.session_state.logged:
 
-    st.title("🚗 Veliora Pro")
-    st.subheader("🎁 Essai gratuit 1 jour")
+   st.markdown("""
+<div style="display:flex; align-items:center; gap:14px; margin-bottom:20px;">
+    
+    <div style="
+        width:42px;
+        height:42px;
+        border-radius:10px;
+        background:linear-gradient(135deg,#0f172a,#1e293b);
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        font-weight:700;
+        font-size:18px;
+        color:white;
+    ">
+        V
+    </div>
+
+    <div>
+        <div style="font-size:26px; font-weight:700;">
+            VELIORA
+        </div>
+        <div style="font-size:14px; color:#9CA3AF;">
+            Cotation automobile intelligente
+        </div>
+    </div>
+
+</div>
+""", unsafe_allow_html=True)
+    st.markdown("### Essai gratuit 1 jour")
 
     st.warning("⚠️ Accès réservé aux professionnels de l’automobile")
     st.info(f"Après 1 jour d'essai : {PRICE_HT}€ HT ({PRICE_TTC}€ TTC) / mois")
