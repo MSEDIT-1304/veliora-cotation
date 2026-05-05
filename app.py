853a59deb4759506bd97712128fe4df7aa224b2a
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-05-05 12:10:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -958,13 +958,13 @@ if calcul:
 
     st.subheader("💰 PRIX MARCHÉ ESTIMÉ")
     st.success(f"{prix_marche_affiche} €")
-    st.caption("(Prix marché estimé basé sur modèle, année et configuration du véhicule.)")
+    st.caption("(Prix marché estimé basé sur prix marché moyen garage.)")
 
     st.markdown(f"📉 Bas : {prix_bas_min} € -> {prix_bas_max} €")
-    st.caption("(2015 à 2018/95000-130000 km peu importe la finition)")
+    st.caption("(km élevés/véhicule ancien.)")
 
     st.markdown(f"📈 Haut : {prix_haut_min} € -> {prix_haut_max} €")
-    st.caption("(2021 à 2025/de 30 à 75000km/ finition luxe.)")
+    st.caption("(véhicule récent/peu de km/finition luxe.)")
     
 
 
