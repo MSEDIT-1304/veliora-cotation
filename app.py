53403480946dfb16e39648d0d1869e25e87ff53b
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-05-08 22:05:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -401,17 +401,12 @@ def ai_price_engine(marque, modele, finition, motorisation, annee, km, carburant
     # correction km réaliste (base dataset = 90 000 km)
 
     if km > 90000:
-        correction_km = -int((km - 90000) * 0.10)
+        correction_km = -int((km - 90000) * 0.06)
 
     else:
-        correction_km = int((90000 - km) * 0.04)
+        correction_km = int((90000 - km) * 0.066)
 
     price = base + correction_km
-
-    st.write("BASE =", base)
-    st.write("KM =", km)
-    st.write("CORRECTION =", correction_km)
-    st.write("PRICE FINAL =", price)
    
 
     return int(price)
