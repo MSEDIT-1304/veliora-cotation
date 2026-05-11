3507d618bc7d4d39999c675cc8ce4716102a7f53
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-05-11 11:58:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3826,7 +3826,7 @@ if calcul:
     prix_estime = round(prix_marche / 10) * 10
 
     bas_affiche = prix_estime 
-    haut_affiche = prix_estime + 1445
+    haut_affiche = prix_estime + 1500
 
 
     # 🔥 STOCKAGE RESULTAT (pour éviter reset)
