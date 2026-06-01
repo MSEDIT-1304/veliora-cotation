70192ae7a422fd71cd93bbb71421939483cca7a2
MSEDIT-1304 <mairy.gillen@gmail.com>
2026-06-01 15:33:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16738,7 +16738,7 @@ if not st.session_state.logged:
 
     st.markdown("<h3 style='margin-bottom:0;'>Veliora Pro</h2>", unsafe_allow_html=True)
     
-    st.info(f"Abonnement : {PRICE_TVAC}€ TVAC) / mois")
+    st.info(f"Abonnement : {PRICE_TVAC}€ TVAC) / mois. Pas de prélèvement mensuel. Paiement pour un mois")
     
     type_client = "Professionnel auto"
     st.success("Compte professionnel requis")
